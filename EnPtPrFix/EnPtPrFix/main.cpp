cfae9cc1fba1efc6bef048b2c835de7187e862a1
Alexander <eve14februar@hotmail.com>
2019-06-13 10:22:28 +0200

    Tiny change

--- EnPtPrFix/EnPtPrFix/main.cpp
+++ EnPtPrFix/EnPtPrFix/main.cpp
@@ -5,11 +5,9 @@ int CALLBACK WinMain( HINSTANCE inst, HINSTANCE previnst, LPSTR cmdline, int cmd
 		Sleep( 1500 );
 		int mouseinfo[ 3 ];
 		BOOL result = SystemParametersInfo( SPI_GETMOUSE, 0, &mouseinfo, 0 );
-		if( result ) {
-			if( mouseinfo[ 2 ] != 0 ) {
-				mouseinfo[ 2 ] = 0;
-				SystemParametersInfo( SPI_SETMOUSE, 0, &mouseinfo, SPIF_SENDCHANGE );
-			}
+		if( result && mouseinfo[ 2 ] != 0 ) { // checking if mouseinfo was pulled, and checking if enhanced pointer precision is on.
+			mouseinfo[ 2 ] = 0;
+			SystemParametersInfo( SPI_SETMOUSE, 0, &mouseinfo, SPIF_SENDCHANGE );
 		}
 	}
 }
